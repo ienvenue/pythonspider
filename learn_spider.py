32e0b8864d152a980a079f1b91b4ea4a8a317fb6
ienvenue <chenyoungjie@163.com>
2021-06-22 18:21:08 +0800

    requests爬取重构

--- learn_spider.py
+++ learn_spider.py
@@ -2,7 +2,7 @@ import requests
 from multiprocessing.dummy import Pool
 import re
 import os
-import time
+from timer import timer
 
 def get_toc(html,start_url):
     toc_url_list=[]
@@ -11,6 +11,12 @@ def get_toc(html,start_url):
     for url in toc_url:
         toc_url_list.append(start_url+'/'+url)
     return toc_url_list
+
+def get_article2(html):
+    chapter_name=re.search('size="4">(.*?)<',html,re.S).group(1)
+    text_block=re.search('<p>(.*?)</p>',html,re.S).group(1)
+    text_block=text_block.replace('<br/>','')
+    return chapter_name,text_block
 
 def get_article(html):
     chapter_name=re.search('size="4">(.*?)<',html,re.S).group(1)
@@ -23,6 +29,22 @@ def save(chapter,article):
     with open(os.path.join('动物农场',chapter+'.txt'),'w',encoding='utf-8') as f:
         f.write(article)
 
+def spider_get_url(start_url):
+    toc_url_list = []
+    html=get_html(start_url)
+    toc_block = re.findall('正文(.*?)</tbody>', html, re.S)[0]
+    toc_url = re.findall('href="(.*?)"', toc_block, re.S)
+    for url in toc_url:
+        toc_url_list.append(start_url+'/'+url)
+    return toc_url_list
+
+def spider_get_article(sites):
+    html = requests.get(sites).content.decode('gbk')
+    chapter_name = re.search('size="4">(.*?)<', html, re.S).group(1)
+    text_block = re.search('<p>(.*?)</p>', html, re.S).group(1)
+    text_block = text_block.replace('<br/>', '')
+    save(chapter_name, text_block)
+
 def get_html(sites):
     # 在下面的代码行中使用断点来调试脚本。
     # 按 Ctrl+F8 切换断点。
@@ -33,6 +55,7 @@ def get_html(sites):
     # print(html_str)
     return html_str
 
+@timer
 def main():
     start_url='http://www.kanunu8.com/book3/6879'
     html=requests.get(start_url).content.decode('GBK')
@@ -41,6 +64,12 @@ def main():
     pool2 = Pool(3)
     pool2.map(get_article,html_str)
 
+@timer
+def main2():
+    start_url = 'http://www.kanunu8.com/book3/6879'
+    pool=Pool(3)
+    pool.map(spider_get_article,spider_get_url(start_url))
 
 if __name__ == '__main__':
-    main()
+    # main()
+    main2()
